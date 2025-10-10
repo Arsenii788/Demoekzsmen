4922b82da6aa4813c105b6687abf3f390f085cbc
kabinet303 <kabinet303@localhost.localdomain>
2025-10-10 11:05:04 +0300

    tyrtyrty

---
+++ moy_ne_sam/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой не сам</title>
+    <link rel ="icon" href="images/dok.jpg">
+    <link rel ="stylesheet" href="images/dok.jpg">
+</head>
+<body>
+    <header> <imG src="images/dok.jpg" alt='логотип'>
+    <h1>Мой не сам<h1>
+</header>
+<footer>2025</footer>
+</body>
+</html>
